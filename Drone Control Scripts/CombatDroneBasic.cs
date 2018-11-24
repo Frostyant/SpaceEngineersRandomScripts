1f890d721c302b1cd3de9cbdc3261a88d1213128
Frostyant <webs.anthony@gmail.com>
2018-11-24 22:37:03 +0000

    Improvments to Basic Combat and Comms

--- Drone Control Scripts/CombatDroneBasic.cs
+++ Drone Control Scripts/CombatDroneBasic.cs
@@ -10,7 +10,16 @@ const string MOVE = "Move"; //Move Orders
 const string DEFEND = "Defend";//Defend order
 const string MAIN = "Main"; //Main fighter loop
 const string CURRENT_ORDER_DATATYPE = "COrder";//This is the Current order Datatype
+const string FALLBACK_DATATYPE = "FBack";//Fback datatype : Fback|GPS
 const float EffectiveRange = (float) 300.0;
+const string HP_DISP_NAME = "HPDisplay";
+const string SYST_HP_DATATYPE = "SystemHp";//datatype for total system Hp
+const string THRUST_HP_DATATYPE = "ThrusterHp";//datatype for thruster system Hp
+const string WEAPONS_HP_DATAPYE = "WeaponHp";//datatype for Weapons systems hp
+const float CritSystHp = (float) 0.7;
+const float CritThustHp = (float) 0.75;
+const float CritWeapHp = (float) 0.5;
+const char InternalSep = '|';
 
 
 #region DETECTION
@@ -18,7 +27,7 @@ const float EffectiveRange = (float) 300.0;
 
 		bool TargetFound = director.HasTarget;//Taken from Rdav Interceptor
 
-		return true;
+		return TargetFound;
 	}
 
 	//Checks if we should engage, returns 0, 1 or a location we have to go to
@@ -181,99 +190,247 @@ const float EffectiveRange = (float) 300.0;
 
 #region Data Handling
 
-/// <summary>
-/// Extracts txt info, splits it and gets relevant DataChunks
-/// </summary>
-/// <param name="txt"></param>
-/// txt panel
-/// <param name="sep"></param>
-/// separator used in split, which distinguishes between the different data chunks
-/// <param name="type"></param>
-/// type of data chunk you want
-/// <param name="InternalSep"></param>
-/// separator used in split, which distinguishes between the info within a data chunk
-/// <returns></returns>
-public List<string> GetData(string txtname, string type, char sep = '\n', char InternalSep = '|')
-{
+	/// <summary>
+	/// Extracts txt info, splits it and gets relevant DataChunks
+	/// </summary>
+	/// <param name="txt"></param>
+	/// txt panel
+	/// <param name="sep"></param>
+	/// separator used in split, which distinguishes between the different data chunks
+	/// <param name="type"></param>
+	/// type of data chunk you want
+	/// <param name="InternalSep"></param>
+	/// separator used in split, which distinguishes between the info within a data chunk
+	/// <returns></returns>
+	public List<string> GetData(string txtname, string type, char sep = '\n', char InternalSep = '|')
+	{
 
-		IMyTextPanel txt = GridTerminalSystem.GetBlockWithName(txtname) as IMyTextPanel;
+			IMyTextPanel txt = GridTerminalSystem.GetBlockWithName(txtname) as IMyTextPanel;
 
-		string[] FullData = GetRef(txt, sep);
-		List<string> output = new List<string>();
+			string[] FullData = GetRef(txt, sep);
+			List<string> output = new List<string>();
 
-		foreach (string DataChunk in FullData)
-		{
-				string[] Data = DataChunk.Split(InternalSep);
-				if (Data[0] == type)
-				{
-						//Adds full Data chunk
-						output.Add(DataChunk);
+			foreach (string DataChunk in FullData)
+			{
+					string[] Data = DataChunk.Split(InternalSep);
+					if (Data[0] == type)
+					{
+							//Adds full Data chunk
+							output.Add(DataChunk);
+					}
+
+			}
+			return output;
+	}
+
+
+	//Removes ALL instances of a datatype
+	public void RemoveDataType(string txtname, string type, char sep = '\n', char InternalSep = '|')
+	{
+
+			IMyTextPanel txt = GridTerminalSystem.GetBlockWithName(txtname) as IMyTextPanel;
+
+			string[] FullData = GetRef(txt, sep);
+
+			if(FullData.Length != 0){
+
+				//convert to list
+				List<string> output = new List<string>(FullData);
+
+				output.RemoveAll(x => x.Split(InternalSep).First().Equals(type));
+
+				LcdClear(txtname);
+
+				foreach(string data in output){
+					LcdPrintln(data,txtname);
 				}
 
-		}
-		return output;
-}
-
-
-//Removes ALL instances of a datatype
-public void RemoveDataType(string txtname, string type, char sep = '\n', char InternalSep = '|')
-{
-
-		IMyTextPanel txt = GridTerminalSystem.GetBlockWithName(txtname) as IMyTextPanel;
-
-		string[] FullData = GetRef(txt, sep);
-
-		if(FullData.Length != 0){
-
-			//convert to list
-			List<string> output = new List<string>(FullData);
-
-			output.RemoveAll(x => x.Split(InternalSep).First().Equals(type));
-
-			LcdClear(txtname);
-
-			foreach(string data in output){
-				LcdPrintln(data,txtname);
 			}
 
+
+	}
+
+	public void AddData(string  txtname ,string type, string data, char sep = '\n', char InternalSep = '|'){
+
+		string Datachunk = type + InternalSep + data + sep;
+
+		LcdPrint(Datachunk,txtname);
+
+	}
+
+		public void LcdPrintln(string msg, string lcdName = "VarPanel")
+		{
+		    LcdPrint(msg + '\n', lcdName);
 		}
 
+		public void LcdPrint(string msg, string lcdName = "VarPanel")
+		{
+		    IMyTextPanel lcd =
+		      GridTerminalSystem.GetBlockWithName(lcdName) as IMyTextPanel;
+		    lcd.WritePublicText(lcd.GetPublicText() + msg);
+		}
 
-}
+		/// <summary>
+		/// Clears selected LCD's public text and returns it.
+		/// </summary>
+		/// <param name="lcdName">Block Name of LCD (not the Title!)</param>
+		/// <returns></returns>
+		public string LcdClear(string lcdName)
+		{
+				IMyTextPanel lcd =
+					GridTerminalSystem.GetBlockWithName(lcdName) as IMyTextPanel;
+				string text = lcd.GetPublicText();
+				lcd.WritePublicText("");
+				return text;
 
-public void AddData(string  txtname ,string type, string data, char sep = '\n', char InternalSep = '|'){
+		}
+#endregion
 
-	string Datachunk = type + InternalSep + data + sep;
+#region HPSystem
+  public void InitHpSystem(){
 
-	LcdPrint(Datachunk,txtname);
+    //initialising
 
-}
+    List<IMyTerminalBlock> AllTerminalBlocks = new List<IMyTerminalBlock>();
 
-	public void LcdPrintln(string msg, string lcdName = "VarPanel")
-	{
-	    LcdPrint(msg + '\n', lcdName);
-	}
+    List<IMyTerminalBlock> AllThrustBlocks = new List<IMyTerminalBlock>();
 
-	public void LcdPrint(string msg, string lcdName = "VarPanel")
-	{
-	    IMyTextPanel lcd =
-	      GridTerminalSystem.GetBlockWithName(lcdName) as IMyTextPanel;
-	    lcd.WritePublicText(lcd.GetPublicText() + msg);
-	}
 
-	/// <summary>
-	/// Clears selected LCD's public text and returns it.
-	/// </summary>
-	/// <param name="lcdName">Block Name of LCD (not the Title!)</param>
-	/// <returns></returns>
-	public string LcdClear(string lcdName)
-	{
-			IMyTextPanel lcd =
-				GridTerminalSystem.GetBlockWithName(lcdName) as IMyTextPanel;
-			string text = lcd.GetPublicText();
-			lcd.WritePublicText("");
-			return text;
+    //Terminal Health
 
+    GridTerminalSystem.GetBlocksOfType<IMyTerminalBlock>(AllTerminalBlocks);
+
+    RemoveDataType(TXT_NAME,SYST_HP_DATATYPE);//just in case
+
+    AddData(TXT_NAME,SYST_HP_DATATYPE,AllTerminalBlocks.Count.ToString());
+
+
+    //Thrust Health
+
+    GridTerminalSystem.GetBlocksOfType<IMyThrust>(AllThrustBlocks);
+
+    RemoveDataType(TXT_NAME,THRUST_HP_DATATYPE);//just in case
+
+    AddData(TXT_NAME,THRUST_HP_DATATYPE,AllThrustBlocks.Count.ToString());
+
+
+    //Weapons Health
+
+    int WeaponsCount = 0;
+
+    foreach(IMyTerminalBlock block in AllTerminalBlocks){
+      if(is_weapon(block)){
+        WeaponsCount += 1;
+      }
+    }
+
+    RemoveDataType(TXT_NAME,WEAPONS_HP_DATAPYE);//just in case
+
+    AddData(TXT_NAME,WEAPONS_HP_DATAPYE,WeaponsCount.ToString());
+  }
+
+  //This is from Phil
+  private bool is_weapon(IMyTerminalBlock block) {
+    if (block is IMySmallMissileLauncher || block is IMySmallMissileLauncherReload ||
+    block is IMyLargeMissileTurret || block is IMyLargeGatlingTurret || block is IMyLargeInteriorTurret ||
+    block is IMySmallGatlingGun) {return true;}
+    else { return false;}
+  }
+
+
+
+  public List<float> CheckHp(char InternalSep = '|'){
+    //initialising
+
+    List<IMyTerminalBlock> AllTerminalBlocks = new List<IMyTerminalBlock>();
+
+    List<IMyTerminalBlock> AllThrustBlocks = new List<IMyTerminalBlock>();
+
+
+    //Terminal Health
+
+    int Hp = 0;
+
+    int WeaponsHp = 0;
+
+    GridTerminalSystem.GetBlocksOfType<IMyTerminalBlock>(AllTerminalBlocks);
+
+    foreach(IMyTerminalBlock block in AllTerminalBlocks){
+      if(block.IsFunctional){
+
+        Hp = Hp + 1;
+
+        if(is_weapon(block)){
+          WeaponsHp = WeaponsHp + 1;
+        }
+
+      }
+
+    }
+
+
+
+    //Thrust Health
+
+    int ThrustHp = 0;
+
+    GridTerminalSystem.GetBlocksOfType<IMyThrust>(AllThrustBlocks);
+
+    foreach(IMyTerminalBlock block in AllThrustBlocks){
+
+      if(block.IsFunctional){
+
+        ThrustHp = ThrustHp +  1;
+
+      }
+
+    }
+
+
+
+    //Getting max hps
+
+    List<string> FullHpString = GetData(TXT_NAME,SYST_HP_DATATYPE);
+
+    List<string> FullThrustString = GetData(TXT_NAME,THRUST_HP_DATATYPE);
+
+    List<string> FullWeaponsString = GetData(TXT_NAME,WEAPONS_HP_DATAPYE);
+
+    int FullHp = Convert.ToInt32(FullHpString[0].Split(InternalSep)[1]);
+
+    int FullThrustHp = Convert.ToInt32(FullThrustString[0].Split(InternalSep)[1]);
+
+    int FullWeaponsHp = Convert.ToInt32(FullWeaponsString[0].Split(InternalSep)[1]);
+
+
+    //Getting percentages
+
+    float HpPerc = (float) Hp / (float) FullHp;
+
+    float ThrustPerc = (float) ThrustHp / (float) FullThrustHp;
+
+    float WeapPerc = (float) WeaponsHp / (float) FullWeaponsHp;
+
+    List<float> AllHps = new List<float>();
+
+
+    AllHps.Add(HpPerc);
+
+    AllHps.Add(ThrustPerc);
+
+    AllHps.Add(WeapPerc);
+
+    return AllHps;
+  }
+
+	public bool CheckHPThreshold(){
+		List<float> Hps = CheckHp();
+
+		if(Hps[0] < CritSystHp || Hps[1] < CritThustHp || Hps[2] < CritWeapHp){
+			return false;
+		}else{
+			return true;
+		}
 	}
 #endregion
 
@@ -289,59 +446,73 @@ public void Main(string ArgumentFull){
 
 	string[] Arguments = ArgumentFull.Split(';');
 
-	IMyRemoteControl cntrl = GridTerminalSystem.GetBlockWithName(CNTRL_NAME) as IMyRemoteControl;
+	//First init AI
+	if(Arguments[0] == "init"){
+		InitHpSystem();
+	}else{
+		IMyRemoteControl cntrl = GridTerminalSystem.GetBlockWithName(CNTRL_NAME) as IMyRemoteControl;
+
+		//Check if we have to retreat due to lack of HP
+		if(CheckHPThreshold()){
+			if(Arguments[0] == "Main"){
+				//Check if we are allowed to engage from orders
+
+				string orders = ShouldIEngage(cntrl);
+				if(orders == "1"){
+					//Amove or Defend in bounds
+					IMyLargeTurretBase director = GridTerminalSystem.GetBlockWithName(TargetingGun) as IMyLargeTurretBase;
 
 
-	if(Arguments[0] == "Main"){
-		//Check if we are allowed to engage
+					if(CheckForTargets(director)){
 
-		string orders = ShouldIEngage(cntrl);
-		if(orders == "1"){
-			//Amove or Defend in bounds
-			IMyLargeTurretBase director = GridTerminalSystem.GetBlockWithName(TargetingGun) as IMyLargeTurretBase;
+		        //If Targets Found Maintain Positiong & Report
+		        //Go to my current position (TO DO : add a bit of randomization for harder targeting by the enemy)
+						Vector3 Enemy = GetTargetVector(director);
+
+						Vector3 Us = cntrl.GetPosition();
+
+						Vector3 EnemyRel = Enemy - Us;//Get Enemy Relative To Us
+
+						Vector3 EnemyDir = EnemyRel/EnemyRel.Length();//Normalizing
+
+						Vector3 BestRange = Enemy - EffectiveRange * EnemyDir;//Moving to Effective Range
+
+		        GoTo(cntrl,BestRange);
+
+		        //Report enemy
+		        ReportEnemy(director);
+					}
+
+				}else if (orders == "0"){
+					//This was a move
+				}else{
+					//Else it was a defend and we were out of bounds
+					GoTo(cntrl,GPStoVector(orders));
+				}
+
+			}else if (Arguments[0] == "AddOrder") {
+				RemoveDataType(TXT_NAME,CURRENT_ORDER_DATATYPE);//Remove Past orders
+
+				AddData(TXT_NAME,CURRENT_ORDER_DATATYPE,Arguments[1]);//Adds New Parameters
 
 
-			if(CheckForTargets(director)){
+				string[] NewOrder = Arguments[1].Split(InternalSep);
 
-        //If Targets Found Maintain Positiong & Report
-        //Go to my current position (TO DO : add a bit of randomization for harder targeting by the enemy)
-				Vector3 Enemy = GetTargetVector(director);
+				if(NewOrder[0] == AMOVE || NewOrder[0] == MOVE){
 
-				Vector3 Us = cntrl.GetPosition();
+					Vector3 vec  = GPStoVector(NewOrder[1]);
 
-				Vector3 EnemyRel = Enemy - Us;//Get Enemy Relative To Us
+					GoTo(cntrl,vec);
 
-				Vector3 EnemyDir = EnemyRel/EnemyRel.Length();//Normalizing
-
-				Vector3 BestRange = Enemy - EffectiveRange * EnemyDir;//Moving to Effective Range
-
-        GoTo(cntrl,BestRange);
-
-        //Report enemy
-        ReportEnemy(director);
+				}
 			}
-
-		}else if (orders == "0"){
-			//This was a move
 		}else{
-			//Else it was a defend and we were out of bounds
-			GoTo(cntrl,GPStoVector(orders));
-		}
+			List<string> Datachunk =  GetData(TXT_NAME,FALLBACK_DATATYPE);
 
-	}else if (Arguments[0] == "AddOrder") {
-		RemoveDataType(TXT_NAME,CURRENT_ORDER_DATATYPE);//Remove Past orders
+			string[] data = Datachunk[0].Split(InternalSep);
 
-		AddData(TXT_NAME,CURRENT_ORDER_DATATYPE,Arguments[1]);//Adds New Parameters
-
-
-		string[] NewOrder = Arguments[1].Split('|');
-
-		if(NewOrder[0] == AMOVE || NewOrder[0] == MOVE){
-
-			Vector3 vec  = GPStoVector(NewOrder[1]);
-
-			GoTo(cntrl,vec);
-
+			//We cannot engage, so we must flee
+			GoTo(cntrl,GPStoVector(data[1]));
 		}
 	}
 
--- Essentials/Comms.cs
+++ Essentials/Comms.cs
@@ -4,6 +4,7 @@
 const string CNTRL_NAME = "cntrl";
 const string TXT_NAME = "memory";
 const string PING_STATUS = "fleet_status";
+const string COMMS_NAME = "comms";
 
 //Initialising Variables
 
@@ -20,93 +21,251 @@ const string PING_STATUS = "fleet_status";
 
 //Functions//
 
+#region BASIC FUNCTIONS
+	public string GetRaw(IMyTextPanel txt){
 
-/// <summary>
-/// Extracts txt info, splits it and gets relevant DataChunks
-/// </summary>
-/// <param name="txt"></param>
-/// txt panel
-/// <param name="sep"></param>
-/// separator used in split, which distinguishes between the different data chunks
-/// <param name="type"></param>
-/// type of data chunk you want
-/// <param name="InternalSep"></param>
-/// separator used in split, which distinguishes between the info within a data chunk
-/// <returns></returns>
-public List<string> GetData(string txtname, string type, char sep = '\n', char InternalSep = ':')
-{
+		//Gets RAW output from text panel (the text)
 
-    IMyTextPanel txt = GridTerminalSystem.GetBlockWithName(txtname) as IMyTextPanel;
+		string output;
 
-    string[] FullData = GetRef(txt, sep);
-    List<string> output = new List<string>();
+		output=txt.GetPublicText();
 
-    foreach (string DataChunk in FullData)
-    {
-        string[] Data = DataChunk.Split(InternalSep);
-        if (Data[0] == type)
-        {
-            //Adds full Data chunk
-            output.Add(DataChunk);
+		return output;
+	}
+
+	public string[] GetRef(IMyTextPanel txt,char sep){
+		//Divides RAW into an array for later use
+
+		string raw;
+
+		raw=GetRaw(txt);
+
+		string[] output =raw.Split(sep);
+
+		return output;
+
+	}
+
+	/// <summary>
+	/// does what is says on the tin
+	/// </summary>
+	/// <param name="gps"></param>
+	/// in GPS format (same as if you would copy past it from a GPS coords in-game)
+	/// <returns></returns>
+	public Vector3 GPStoVector(string gps)
+	{
+
+			string[] coords;
+
+			//Getting Target Position//
+
+			coords = gps.Split(':');
+
+			//Pending, need to change indices
+
+			Vector3 gpsvector = new Vector3(Convert.ToSingle(coords[2]),
+															 Convert.ToSingle(coords[3]),
+															 Convert.ToSingle(coords[4]));
+
+			return gpsvector;
+	}
+
+	/// <summary>
+	/// Does what it says on the tin
+	/// </summary>
+	/// <param name="vec"></param>
+	/// <returns></returns>
+	public string VectorToGPS(Vector3 vec)
+	{
+			string output;
+
+			output = "GPS:location:"
+					+ Convert.ToString(vec.X) + ":"
+					+ Convert.ToString(vec.Y) + ":"
+					+ Convert.ToString(vec.Z) + ":";
+
+			return output;
+	}
+
+	public void ExecuteSimple(string TIMER_NAME){
+		//Starts a Timer Block
+
+		IMyTimerBlock timer = GridTerminalSystem.GetBlockWithName(TIMER_NAME) as IMyTimerBlock;
+
+
+		if(timer != null){
+
+
+			ITerminalAction start;
+
+			start=timer.GetActionWithName("Start");
+
+			start.Apply(timer);
+		}
+	}
+
+	//For Remote Controls, makes you go to Pos
+	public void GoTo(IMyRemoteControl cntrl, Vector3 Pos, bool Relative = false){
+		//orders ship to move to Vector location
+		Vector3 obj;
+
+		if (Relative == true){
+				obj = Pos + cntrl.GetPosition();
+		}else{
+			obj=Pos;
+		}
+
+		cntrl.ClearWaypoints();
+		cntrl.AddWaypoint(obj,"obj");
+		cntrl.SetAutoPilotEnabled(true);
+	}
+
+	public Vector3 GetBlockPosition(string BLOCK_NAME){
+		//Returns Block Position//
+
+		Vector3 output;
+
+		IMyTerminalBlock block = GridTerminalSystem.GetBlockWithName(BLOCK_NAME) as IMyTerminalBlock;
+
+		output = block.GetPosition();
+
+		return output;
+	}
+#endregion
+
+
+#region Data Handling
+  //Checks Given ID
+  public bool IDCheck(string given){
+    string MyID = Me.CubeGrid.EntityId.ToString();
+
+    if(MyID == given){
+      return true;
+    }else{/*
+      List<string> groups = GetData(TXT_NAME,"group",sep,InternalSep);
+      foreach(string group in groups){
+        string[] data = DataChunk.Split(InternalSep);
+        if(data[1] == given){
+          return true;
         }
-
-    }
-    return output;
-}
-
-public bool SendMessage(string target,string rawmessage,char sep = '\n', char InternalSep = ':'){
-
-  string MyId = Me.CubeGrid.EntityId;
-
-  string message ="MSG"+';'+ MyId + ';' + target + ';' + rawmessage;//note raw message should use same separator (ie no internals, so ;)
-
-  return TransmitMessage( message, MyTransmitTarget target = MyTransmitTarget.Default );
-}
-
-public bool IDCheck(string given){
-  string MyID = Me.CubeGrid.EntityId;
-
-  if(MyID == given){
-    return true;
-  }else{
-    List<string> groups = GetData(TXT_NAME,"group",sep,InternalSep);
-    foreach(string group in groups){
-      string[] data = DataChunk.Split(InternalSep);
-      if(data[1] == given){
-        return true;
       }
+
+    }else{*/
+      return false;
     }
-  }else{
-    return false;
   }
-}
 
-public void LcdPrintln(string msg, string lcdName = "VarPanel")
-{
-    LcdPrint(msg + '\n', lcdName);
-}
 
-public void LcdPrint(string msg, string lcdName = "VarPanel")
-{
-    IMyTextPanel lcd =
-      GridTerminalSystem.GetBlockWithName(lcdName) as IMyTextPanel;
-    lcd.WritePublicText(lcd.GetPublicText() + msg);
-}
+	/// <summary>
+	/// Extracts txt info, splits it and gets relevant DataChunks
+	/// </summary>
+	/// <param name="txt"></param>
+	/// txt panel
+	/// <param name="sep"></param>
+	/// separator used in split, which distinguishes between the different data chunks
+	/// <param name="type"></param>
+	/// type of data chunk you want
+	/// <param name="InternalSep"></param>
+	/// separator used in split, which distinguishes between the info within a data chunk
+	/// <returns></returns>
+	public List<string> GetData(string txtname, string type, char sep = '\n', char InternalSep = '|')
+	{
 
-/// <summary>
-/// Clears selected LCD's public text and returns it.
-/// </summary>
-/// <param name="lcdName">Block Name of LCD (not the Title!)</param>
-/// <returns></returns>
-public string LcdClear(string lcdName)
-{
-    IMyTextPanel lcd =
-      GridTerminalSystem.GetBlockWithName(lcdName) as IMyTextPanel;
-    string text = lcd.GetPublicText();
-    lcd.WritePublicText("");
-    return text;
+			IMyTextPanel txt = GridTerminalSystem.GetBlockWithName(txtname) as IMyTextPanel;
 
-}
+			string[] FullData = GetRef(txt, sep);
+			List<string> output = new List<string>();
+
+			foreach (string DataChunk in FullData)
+			{
+					string[] Data = DataChunk.Split(InternalSep);
+					if (Data[0] == type)
+					{
+							//Adds full Data chunk
+							output.Add(DataChunk);
+					}
+
+			}
+			return output;
+	}
+
+
+	//Removes ALL instances of a datatype
+	public void RemoveDataType(string txtname, string type, char sep = '\n', char InternalSep = '|')
+	{
+
+			IMyTextPanel txt = GridTerminalSystem.GetBlockWithName(txtname) as IMyTextPanel;
+
+			string[] FullData = GetRef(txt, sep);
+
+			if(FullData.Length != 0){
+
+				//convert to list
+				List<string> output = new List<string>(FullData);
+
+				output.RemoveAll(x => x.Split(InternalSep).First().Equals(type));
+
+				LcdClear(txtname);
+
+				foreach(string data in output){
+					LcdPrintln(data,txtname);
+				}
+
+			}
+
+
+	}
+
+	public void AddData(string  txtname ,string type, string data, char sep = '\n', char InternalSep = '|'){
+
+		string Datachunk = type + InternalSep + data + sep;
+
+		LcdPrint(Datachunk,txtname);
+
+	}
+
+		public void LcdPrintln(string msg, string lcdName = "VarPanel")
+		{
+		    LcdPrint(msg + '\n', lcdName);
+		}
+
+		public void LcdPrint(string msg, string lcdName = "VarPanel")
+		{
+		    IMyTextPanel lcd =
+		      GridTerminalSystem.GetBlockWithName(lcdName) as IMyTextPanel;
+		    lcd.WritePublicText(lcd.GetPublicText() + msg);
+		}
+
+		/// <summary>
+		/// Clears selected LCD's public text and returns it.
+		/// </summary>
+		/// <param name="lcdName">Block Name of LCD (not the Title!)</param>
+		/// <returns></returns>
+		public string LcdClear(string lcdName)
+		{
+				IMyTextPanel lcd =
+					GridTerminalSystem.GetBlockWithName(lcdName) as IMyTextPanel;
+				string text = lcd.GetPublicText();
+				lcd.WritePublicText("");
+				return text;
+
+		}
+#endregion
+
+#region comms
+  public bool SendMessage(string target,string rawmessage){
+
+    string MyId = Me.CubeGrid.EntityId.ToString();
+
+    string message ="MSG"+';'+ MyId + ';' + target + ';' + rawmessage;//note raw message should use same separator (ie no internals, so ;)
+
+    IMyRadioAntenna radio = GridTerminalSystem.GetBlockWithName(COMMS_NAME) as IMyRadioAntenna;
+
+    return radio.TransmitMessage( message, MyTransmitTarget.Default );
+  }
+#endregion
+
+
 
 
 
@@ -127,7 +286,7 @@ public void Main(string ArgumentFull){
         if(input[3] == "PNGR"){
           bool Success = SendMessage(input[1],"PNG");//todo : ADD dmg report(ie integrity)
         }else if (input[3] == "PNG") {
-          LcdPrintln(input[3],PING_STATUS,)
+          LcdPrintln(input[3],PING_STATUS);
         }
       }
 
